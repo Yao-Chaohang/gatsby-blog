d03cfc263d6f5da636f878ab51976902bf9bb31f
Yao-Chaohang <chaohang.yao@foodsup.com>
2023-05-24 15:32:53 +0800

    添加antd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from "react"
+import { Button } from 'antd'
 
 const IndexPage = () => {
   return (
     <main>
-      <h1 className="text-3xl text-blue-500 font-bold underline">我的gatsby第一个站点！</h1> 
+      <h1 className="text-3xl text-blue-500 font-bold underline">我的gatsby第一个站点！</h1>
+      <Button>Primary Button</Button>
     </main>
   )
 }
