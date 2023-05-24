c5f09e4e58bc8e5ee0e6d06ff0fb347607f3bc70
Yao-Chaohang <chaohang.yao@foodsup.com>
2023-05-24 15:52:33 +0800

    添加sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-postcss',
-    'gatsby-plugin-layout'
+    'gatsby-plugin-layout',
+    'gatsby-plugin-sass'
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 import { Button } from 'antd'
+import '../styles/index.scss'
 
 const IndexPage = () => {
   return (
     <main>
       <h1 className="text-3xl text-blue-500 font-bold underline">我的gatsby第一个站点！</h1>
       <Button>Primary Button</Button>
+      <h3 className="test">测试文字</h3>
     </main>
   )
 }
