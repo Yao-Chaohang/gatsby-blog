f5349d7830685b4ce4ed79bf45f823958a15e595
Yao-Chaohang <chaohang.yao@foodsup.com>
2023-05-31 17:15:34 +0800

    删除访问统计

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,13 +2,6 @@ import * as React from "react"
 import Header from '../components/Header'
 
 const Layout = ({ children }) => {
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?e78f173e5720d8454b9f51ec8e34d89a";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-    })();
-
     return (
         <div>
             <Header />
