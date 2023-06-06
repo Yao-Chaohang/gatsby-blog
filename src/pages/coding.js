2654994db1abb6fb2ec623122a60d90c62aae30c
Yao-Chaohang <chaohang.yao@foodsup.com>
2023-06-06 17:35:59 +0800

    添加代码人生和生活圈的页面内容

--- src/pages/coding.js
+++ src/pages/coding.js
@@ -1,8 +1,61 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import ContentCard from "../components/ContentCard"
 
 const Coding = () => {
+    const { allMarkdownRemark } = useStaticQuery(graphql`
+        query {
+        allMarkdownRemark(filter: {frontmatter: {categories: {eq: "coding"}}}) {
+            edges {
+            node {
+                frontmatter {
+                date(formatString: "YYYY-MM-DD")
+                title
+                author
+                tags
+                categories
+                }
+                id
+                excerpt(format: PLAIN, pruneLength: 80)
+            }
+            }
+            nodes {
+            fields {
+                slug
+            }
+            }
+        }
+        }
+    `)
+
+    const contentList = allMarkdownRemark.edges
+    contentList.forEach((item, index) => {
+        contentList[index].node.slug = allMarkdownRemark.nodes[index].fields.slug
+    })
+
     return (
-        <div>代码人生</div>
+        <div className='coding'>
+            {
+                contentList && contentList.map((item, index) => {
+                    return (
+                    <ContentCard key={index}
+                        name={item.node.frontmatter.author}
+                        title={item.node.frontmatter.title}
+                        time={item.node.frontmatter.date}
+                        source={item.node.frontmatter.categories}
+                        tags={item.node.frontmatter.tags}
+                        slug={item.node.slug}
+                    >
+                        {
+                        {
+                            'description': <span>{item.node.excerpt}</span>
+                        }
+                        }
+                    </ContentCard>
+                    )
+                })
+            }
+        </div>
     )
 }
 
--- src/pages/everyday.js
+++ src/pages/everyday.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import ContentCard from "../components/ContentCard"
 
 const Everyday = () => {
+    const { allMarkdownRemark } = useStaticQuery(graphql`
+        query {
+        allMarkdownRemark(filter: {frontmatter: {categories: {eq: "everyday"}}}) {
+            edges {
+            node {
+                frontmatter {
+                date(formatString: "YYYY-MM-DD")
+                title
+                author
+                tags
+                categories
+                }
+                id
+                excerpt(format: PLAIN, pruneLength: 80)
+            }
+            }
+            nodes {
+            fields {
+                slug
+            }
+            }
+        }
+        }
+    `)
+
+    const contentList = allMarkdownRemark.edges
+    contentList.forEach((item, index) => {
+        contentList[index].node.slug = allMarkdownRemark.nodes[index].fields.slug
+    })
+
     return (
-        <div>
-            生活圈
+        <div className='everyday'>
+            {
+                contentList && contentList.map((item, index) => {
+                    return (
+                    <ContentCard key={index}
+                        name={item.node.frontmatter.author}
+                        title={item.node.frontmatter.title}
+                        time={item.node.frontmatter.date}
+                        source={item.node.frontmatter.categories}
+                        tags={item.node.frontmatter.tags}
+                        slug={item.node.slug}
+                    >
+                        {
+                        {
+                            'description': <span>{item.node.excerpt}</span>
+                        }
+                        }
+                    </ContentCard>
+                    )
+                })
+            }
         </div>
     )
 }
