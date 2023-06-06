1419d780ba4f3d67a4c0d5d5ae9beff9e95f9e54
Yao-Chaohang <chaohang.yao@foodsup.com>
2023-06-06 11:57:25 +0800

    添加markdown支持

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,51 @@
 import * as React from "react"
+import { graphql, useStaticQuery } from 'gatsby'
 import ContentCard from "../components/ContentCard"
 import '../styles/index.scss'
 
 const IndexPage = () => {
+  const { allMarkdownRemark: { edges } } = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              date
+              title
+              author
+              tags
+              categories
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const contentList = edges
+
   return (
     <main className="home">
       <div className="content">
-        <ContentCard name='超航' title="标题占位title" time='2023-06-05' source='代码人生' tags={['标签1', '标签2']}>
-          {
-            {
-              'description': <span>描述信息</span>
-            }
-          }
-        </ContentCard>
+        {
+          contentList && contentList.map((item, index) => {
+            return (
+              <ContentCard key={index}
+                name={item.node.frontmatter.author}
+                title={item.node.frontmatter.title}
+                time={item.node.frontmatter.date}
+                source={item.node.frontmatter.categories}
+                tags={item.node.frontmatter.tags}
+              >
+                {
+                  {
+                    'description': <span>描述信息</span>
+                  }
+                }
+              </ContentCard>
+            )
+          })
+        }
       </div>
     </main>
   )
