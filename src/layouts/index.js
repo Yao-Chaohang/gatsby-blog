9832e68c399b653343c48de27a999ccea61b44ee
Yao-Chaohang <chaohang.yao@foodsup.com>
2023-06-07 17:02:14 +0800

    把antd默认语言改为中文

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,8 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import UserInfo from "../components/UserInfo"
 import Card from '../components/Card'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
 import { CalendarOutlined, FileTextOutlined } from '@ant-design/icons'
 import '../styles/layout.scss'
 import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
@@ -50,51 +52,53 @@ const Layout = ({ children }) => {
     const announcement = nodes[1].announcement
 
     return (
-        <div className="layout">
-            <Header />
-            <Banner />
-            <div className="layout-body">
-                <div className='website-left'>
-                    <UserInfo />
-                    <div className="site-info-left">
-                        <Card title="站点公告" english-title="Site announcement">
-                            <div className="announcement">
-                                <ul>
-                                    {
-                                        announcement && announcement.map(item => {
-                                            return (
-                                                <li key={item.content}>
-                                                    <CalendarOutlined />
-                                                    {item.time}  {item.content.length > 15 ? item.content.substring(0, 14) + '...' : item.content}
-                                                </li>
-                                            )
-                                        })
-                                    }
-                                </ul>
-                            </div>
-                        </Card>
-                        <Card title="最新文章" english-title="Latest article">
-                            <div className="article-list">
-                                <ul>
-                                    {
-                                        contentList && contentList.map((item, index) => {
-                                            return (
-                                                <li key={index}>
-                                                    <FileTextOutlined />
-                                                    <span>{item.node.frontmatter.date}  </span>
-                                                    <span>{item.node.frontmatter.title.length > 15 ? item.node.frontmatter.title.substring(0, 14) + '...' : item.node.frontmatter.title}</span>
-                                                </li>
-                                            )
-                                        })
-                                    }
-                                </ul>
-                            </div>
-                        </Card>
+        <ConfigProvider locale={zhCN}>
+            <div className="layout">
+                <Header />
+                <Banner />
+                <div className="layout-body">
+                    <div className='website-left'>
+                        <UserInfo />
+                        <div className="site-info-left">
+                            <Card title="站点公告" english-title="Site announcement">
+                                <div className="announcement">
+                                    <ul>
+                                        {
+                                            announcement && announcement.map(item => {
+                                                return (
+                                                    <li key={item.content}>
+                                                        <CalendarOutlined />
+                                                        {item.time}  {item.content.length > 15 ? item.content.substring(0, 14) + '...' : item.content}
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                            </Card>
+                            <Card title="最新文章" english-title="Latest article">
+                                <div className="article-list">
+                                    <ul>
+                                        {
+                                            contentList && contentList.map((item, index) => {
+                                                return (
+                                                    <li key={index}>
+                                                        <FileTextOutlined />
+                                                        <span>{item.node.frontmatter.date}  </span>
+                                                        <span>{item.node.frontmatter.title.length > 15 ? item.node.frontmatter.title.substring(0, 14) + '...' : item.node.frontmatter.title}</span>
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                            </Card>
+                        </div>
                     </div>
+                    <div className='website-right'>{children}</div>
                 </div>
-                <div className='website-right'>{children}</div>
             </div>
-        </div>
+        </ConfigProvider>
     )
 }
 
