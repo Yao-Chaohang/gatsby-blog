8fd95c472ffbb2e8d8faddeb4d8356a2585a5900
Yao-Chaohang <chaohang.yao@foodsup.com>
2023-06-07 17:24:31 +0800

    添加标签云

--- src/pages/tag-cloud.js
+++ src/pages/tag-cloud.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import BreadcrumbSite from '../components/Breadcrumb'
+import { graphql, useStaticQuery } from 'gatsby'
+import { Tag } from 'antd'
+import { TagsOutlined } from '@ant-design/icons'
+import '../styles/tag-cloud.scss'
 
 const TagCloud = () => {
+    const { allMarkdownRemark } = useStaticQuery(graphql`
+        query {
+        allMarkdownRemark(filter: {frontmatter: {categories: {eq: "coding"}}}) {
+            edges {
+                node {
+                    frontmatter {
+                        tags
+                    }
+                }
+            }
+        }
+        }
+    `)
+
+    const { edges } = allMarkdownRemark
+    const tags = [].concat(...edges.map(edge => edge.node.frontmatter.tags))
+
     return (
-        <div>
-            标签云
-        </div>
+        <>
+            <BreadcrumbSite classification="tag-cloud" />
+            <div className='tag-cloud'>
+                {
+                    tags && tags.map((tag, index) => (<Tag key={index} icon={<TagsOutlined />}>{tag}</Tag>))
+                }
+            </div>
+        </>
     )
 }
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,8 @@ export const formatType = (type) => {
             return '代码人生'
         case 'everyday':
             return '生活圈'
+        case 'tag-cloud':
+            return '标签云'
         default:
             return ''
     }
